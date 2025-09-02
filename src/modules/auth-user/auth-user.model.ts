ddd6bff8f9b01d3c3f0363fa5c9f62b226f6e25d
Rashmi Panda <rrashmipanda2400@gmail.com>
2025-09-02 22:02:22 +0530

    bcrypt issue fixed

--- src/modules/auth-user/auth-user.model.ts
+++ src/modules/auth-user/auth-user.model.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema, Document } from "mongoose";
-// import bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
 // import jwt from "jsonwebtoken";
 // import crypto from "crypto";
 
