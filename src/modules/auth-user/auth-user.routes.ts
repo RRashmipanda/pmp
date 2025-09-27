dac5bf13988c021718d3fb4af45b6f6580b58094
Rashmi Panda <rrashmipanda2400@gmail.com>
2025-09-27 15:12:48 +0530

    auth route updated

--- src/modules/auth-user/auth-user.routes.ts
+++ src/modules/auth-user/auth-user.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import * as controller from './auth-user.controller';
 import { validate } from "../../middlewares/validator-middleware";
-import { userForgotPasswordValidator, userLoginValidator,userRegisterValidator, userResetForgotPasswordValidator } from "../../validators";
+import { userChangeCurrentPasswordValidator, userForgotPasswordValidator, userLoginValidator,userRegisterValidator, userResetForgotPasswordValidator } from "../../validators";
 
 const router = Router();
 
@@ -15,8 +15,10 @@ router.post("/forgot-password",userForgotPasswordValidator,validate,controller.f
 router.post("/reset-password/:resetToken",userResetForgotPasswordValidator,validate,controller.resetForgotPassword);
 
 
-router.get("/logout",controller.logoutUser);
+router.post("/logout",controller.logoutUser);
 router.get("/me", controller.getCurrentUser);
+router.post("/change-password",userChangeCurrentPasswordValidator(),validate,controller.changeCurrentPassword);
+router.post("/resend-email-verification",controller.resendEmailVerification);
 
 
 
